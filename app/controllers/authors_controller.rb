3e6fb871f783f00f9fdf89bb85fe1e93664073f6
Omar Branez <omar.branez@gmail.com>
2021-06-08 14:28:33 -0400

    commit tree 7bda1157010367aade258b8759b621e5938891c4

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,9 +8,25 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @author = Author.find(params[:id])
+  end
 
-    redirect_to author_path(@author)
+  def update
+    @author = Author.find(params[:id])
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
 
   private
@@ -18,4 +34,5 @@ class AuthorsController < ApplicationController
   def author_params
     params.permit(:name, :email, :phone_number)
   end
+
 end
